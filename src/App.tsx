47a14f59d98c7728747cdc6ed90af8281f0633d4
Diego Ballesteros <diballesterosc@gmail.com>
2021-06-09 17:52:40 -0500

    Initial app layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,58 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Tracker from 'components/Tracker/Tracker';
+import { ReactComponent as Menu } from 'assets/svg/menu.svg';
+import { ReactComponent as ArrowLeft } from 'assets/svg/arrow-left.svg';
+import styles from './App.module.scss';
 
-function App() {
+const App: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className="App">
-      <header>This is the header</header>
-      <div>This is where the tabs go</div>
-      <div>This is where the table goes</div>
+    <div className={styles.app}>
+      <AppBar className={`${styles.appBar} ${open ? styles.appBarShift : ''}`}>
+        <Toolbar>
+          {!open && (
+            <IconButton
+              aria-label="open drawer"
+              color="inherit"
+              className={styles.menuIcon}
+              onClick={handleOpen}
+              edge="start"
+            >
+              <Menu />
+            </IconButton>
+          )}
+          <h1>Nuzlocke Tracker</h1>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        className={`${styles.drawer} ${open ? styles.open : styles.close}`}
+        classes={{ paper: open ? styles.open : styles.close }}
+      >
+        <div className={styles.arrowContainer}>
+          <IconButton onClick={handleClose}>
+            <ArrowLeft />
+          </IconButton>
+        </div>
+      </Drawer>
+      <main className={styles.main}>
+        <Tracker />
+      </main>
     </div>
   );
-}
+};
 
 export default App;
