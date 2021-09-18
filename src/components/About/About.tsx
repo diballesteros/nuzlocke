7d4f2c8424c03c7e471f44295229900ee09b3344
Diego Ballesteros <diballesterosc@gmail.com>
2021-09-18 07:00:53 -0500

    Changelog, about updates, version about, included amazon appstore link

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ReactComponent as MicrosoftSVG } from 'assets/svg/English_get.svg';
 import kofi from 'assets/img/kofi2.png';
 import google from 'assets/img/google-play-badge.png';
+import amazon from 'assets/img/amazon-appstore-badge-english-white.png';
 import { Page } from 'common';
 import styles from './About.module.scss';
 
@@ -11,8 +12,9 @@ const About: React.FC = () => {
       <div className={styles.about}>
         <p>
           Thank you for using Nuzlocke Tracker! Me and fellow contributors are actively improving
-          this app in our free time. If you like the app please rate it in the Microsoft Store or
-          Google Play. Also, check out ways to <b>optionally</b> support the app below.
+          this app in our free time. If you like the app please rate it in the Microsoft Store,
+          Google Play or the Amazon Appstore. Also, check out ways to <b>optionally</b> support the
+          app below.
         </p>
         <h3>Credits:</h3>
         <ul className={styles.credits}>
@@ -122,8 +124,17 @@ const About: React.FC = () => {
           <a
             className={styles.playstore}
             href="https://play.google.com/store/apps/details?id=app.netlify.nuzlocke.twa&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
+            rel="noopener noreferrer"
+            target="_blank"
           >
             <img alt="Get it on Google Play" src={google} />
+          </a>
+          <a
+            href="https://www.amazon.com/dp/B09GFF7VCB/ref=apps_sf_sta"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <img alt="Get it on Amazon appstore" src={amazon} />
           </a>
         </div>
       </div>
