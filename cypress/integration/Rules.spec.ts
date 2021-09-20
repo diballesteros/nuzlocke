da7a5198468316ab06250885cfb444c65cada19a
Diego Ballesteros <diballesterosc@gmail.com>
2021-09-19 23:30:12 -0500

    Added route 46, error handling tests for webshare, changelog updates

--- cypress/integration/Rules.spec.ts
+++ cypress/integration/Rules.spec.ts
@@ -62,71 +62,73 @@ describe('Rules', () => {
     cy.contains('Nuzlocke Custom').should('not.exist');
   });
 
-  it('Smart Rules of type', () => {
-    cy.get('[data-testid=add-rule]').click();
-    cy.contains('Type').click();
-    cy.get('[data-testid=add-rule-type]').click();
-    cy.contains('GRASS').click();
-    cy.contains('Add Rule').click();
-    cy.contains('Save').click();
-    cy.contains('Allowed types: GRASS').should('exist');
-    cy.get('[data-testid=edit-rule-3]').click();
-    cy.contains('Cancel').click();
-    cy.get('[data-testid=edit-rule-3]').click();
-    cy.get('[data-testid=edit-rule-type]').click();
-    cy.contains('ICE').click();
-    cy.contains('Edit Rule').click();
-    cy.contains('Save').click();
-    cy.get('[data-testid=game-select]').click();
-    cy.contains('Sword and Shield').click();
-    cy.get('[data-testid=options]').click();
-    cy.get('.vertical > :nth-child(1)').click();
-    cy.get('[data-testid=pokemon-0]').click();
-    cy.contains('Scorbunny').click();
-    cy.contains('FORBIDDEN TYPE').should('exist');
-  });
+  context('Smart Rules', () => {
+    it('Smart Rules of type', () => {
+      cy.get('[data-testid=add-rule]').click();
+      cy.contains('Type').click();
+      cy.get('[data-testid=add-rule-type]').click();
+      cy.contains('GRASS').click();
+      cy.contains('Add Rule').click();
+      cy.contains('Save').click();
+      cy.contains('Allowed types: GRASS').should('exist');
+      cy.get('[data-testid=edit-rule-3]').click();
+      cy.contains('Cancel').click();
+      cy.get('[data-testid=edit-rule-3]').click();
+      cy.get('[data-testid=edit-rule-type]').click();
+      cy.contains('ICE').click();
+      cy.contains('Edit Rule').click();
+      cy.contains('Save').click();
+      cy.get('[data-testid=game-select]').click();
+      cy.contains('Sword and Shield').click();
+      cy.get('[data-testid=options]').click();
+      cy.get('.vertical > :nth-child(1)').click();
+      cy.get('[data-testid=pokemon-0]').click();
+      cy.contains('Scorbunny').click();
+      cy.contains('FORBIDDEN TYPE').should('exist');
+    });
 
-  it('Smart Rules of generation', () => {
-    cy.get('[data-testid=add-rule]').click();
-    cy.contains('Generation').click();
-    cy.get('[data-testid=add-rule-generation]').click();
-    cy.get('[data-testid=add-rule-generation]').contains(1).click();
-    cy.contains('Add Rule').click();
-    cy.contains('Save').click();
-    cy.contains('Allowed generations: 1').should('exist');
-    cy.get('[data-testid=edit-rule-3]').click();
-    cy.get('[data-testid=edit-rule-generation]').click();
-    cy.contains('6').click();
-    cy.contains('Edit Rule').click();
-    cy.contains('Save').click();
-    cy.get('[data-testid=game-select]').click();
-    cy.contains('Sword and Shield').click();
-    cy.get('[data-testid=options]').click();
-    cy.get('.vertical > :nth-child(1)').click();
-    cy.get('[data-testid=pokemon-0]').click();
-    cy.contains('Scorbunny').click();
-    cy.contains('FORBIDDEN GEN').should('exist');
-  });
+    it('Smart Rules of generation', () => {
+      cy.get('[data-testid=add-rule]').click();
+      cy.contains('Generation').click();
+      cy.get('[data-testid=add-rule-generation]').click();
+      cy.get('[data-testid=add-rule-generation]').contains(1).click();
+      cy.contains('Add Rule').click();
+      cy.contains('Save').click();
+      cy.contains('Allowed generations: 1').should('exist');
+      cy.get('[data-testid=edit-rule-3]').click();
+      cy.get('[data-testid=edit-rule-generation]').click();
+      cy.contains('6').click();
+      cy.contains('Edit Rule').click();
+      cy.contains('Save').click();
+      cy.get('[data-testid=game-select]').click();
+      cy.contains('Sword and Shield').click();
+      cy.get('[data-testid=options]').click();
+      cy.get('.vertical > :nth-child(1)').click();
+      cy.get('[data-testid=pokemon-0]').click();
+      cy.contains('Scorbunny').click();
+      cy.contains('FORBIDDEN GEN').should('exist');
+    });
 
-  it('Smart Rules of level', () => {
-    cy.get('[data-testid=add-rule]').click();
-    cy.contains('Level').click();
-    cy.get('[data-testid=add-rule-level-input] > input').type('1');
-    cy.contains('Save').click();
-    cy.contains('Max level 1').should('exist');
-    cy.get('[data-testid=edit-rule-3]').click();
-    cy.get('[data-testid=edit-rule-level-input] > input').clear().type('2');
-    cy.contains('Save').click();
-    cy.get('[data-testid=game-select]').click();
-    cy.contains('Sword and Shield').click();
-    cy.get('[data-testid=options]').click();
-    cy.get('.vertical > :nth-child(1)').click();
-    cy.get('[data-testid=pokemon-0]').click();
-    cy.contains('Scorbunny').click();
-    cy.get('[data-testid=edit-encounter-0]').click();
-    cy.get('[data-testid=level] > input').type('5');
-    cy.contains('Save').click();
-    cy.contains('OVERLEVELED').should('exist');
+    it('Smart Rules of level', () => {
+      cy.get('[data-testid=add-rule]').click();
+      cy.contains('Level').click();
+      cy.get('[data-testid=add-rule-level-input] > input').type('1');
+      cy.contains('Save').click();
+      cy.contains('Max level 1').should('exist');
+      cy.get('[data-testid=edit-rule-3]').click();
+      cy.get('[data-testid=edit-rule-level-input] > input').clear().type('2');
+      cy.contains('Save').click();
+      cy.get('[data-testid=game-select]').click();
+      cy.contains('Sword and Shield').click();
+      cy.get('[data-testid=options]').click();
+      cy.get('.vertical > :nth-child(1)').click();
+      cy.get('[data-testid=pokemon-0]').click();
+      cy.contains('Scorbunny').click();
+      cy.get('[data-testid=edit-encounter-0]').click();
+      cy.get('[data-testid=level] > input').type('5');
+      cy.contains('Save').click();
+      cy.contains('OVERLEVELED').should('exist');
+    });
   });
 
   it('Over 6 alert', { scrollBehavior: 'center' }, () => {
@@ -188,21 +190,40 @@ describe('Rules', () => {
     cy.get('.page').click(1, 1);
   });
 
-  it('Share Image - WebShare', { browser: '!firefox' }, () => {
-    cy.visit('/', {
-      onBeforeLoad(win) {
-        (win.navigator as ExtendedNavigator).canShare = () => {
-          return true;
-        };
-        delete win.navigator.share;
-        delete (win.navigator as ExtendedNavigator).canShare;
-        (win.navigator as ExtendedNavigator).canShare = cy.stub().resolves(true);
-        win.navigator.share = cy.stub().resolves(true);
-      },
+  context('Webshare', () => {
+    it('Share Rules - WebShare', { browser: '!firefox' }, () => {
+      cy.visit('/', {
+        onBeforeLoad(win) {
+          (win.navigator as ExtendedNavigator).canShare = () => {
+            return true;
+          };
+          delete win.navigator.share;
+          delete (win.navigator as ExtendedNavigator).canShare;
+          (win.navigator as ExtendedNavigator).canShare = cy.stub().resolves(true);
+          win.navigator.share = cy.stub().resolves(true);
+        },
+      });
+      cy.get('[data-testid=options]').click();
+      cy.contains('Rules').click();
+      cy.get('h1').click();
+      cy.get('[data-testid=share-encounters]').click();
     });
-    cy.get('[data-testid=options]').click();
-    cy.contains('Rules').click();
-    cy.get('h1').click();
-    cy.get('[data-testid=share-encounters]').click();
+
+    it('Share Rules - WebShare - Error', { browser: '!firefox' }, () => {
+      cy.visit('/', {
+        onBeforeLoad(win) {
+          delete win.navigator.share;
+          delete (win.navigator as ExtendedNavigator).canShare;
+          (win.navigator as ExtendedNavigator).canShare = () => true;
+          win.navigator.share = cy.stub().rejects(Error('test'));
+          cy.stub(win.console, 'error').as('consoleError');
+        },
+      });
+      cy.get('[data-testid=options]').click();
+      cy.contains('Rules').click();
+      cy.get('h1').click();
+      cy.get('[data-testid=share-encounters]').click();
+      cy.get('@consoleError').should('be.calledOnce');
+    });
   });
 });
