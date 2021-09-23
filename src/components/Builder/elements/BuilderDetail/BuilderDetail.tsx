3c72b2ef9363d2da6e390a1161425e227195b98b
Diego Ballesteros <diballesterosc@gmail.com>
2021-09-23 07:27:46 -0500

    conflict res

--- src/components/Builder/elements/BuilderDetail/BuilderDetail.tsx
+++ src/components/Builder/elements/BuilderDetail/BuilderDetail.tsx
@@ -1,8 +1,8 @@
-import React, { useCallback, useEffect, useMemo } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import useStore from 'store';
 import { Page } from 'common';
-import styles from './BuilderDetail.module.scss';
+// import styles from './BuilderDetail.module.scss';
 
 const BuilderDetail: React.FC = () => {
   const { game } = useParams<{ game: string }>();
@@ -20,7 +20,7 @@ const BuilderDetail: React.FC = () => {
     }
   }, [history, team]);
 
-  const test = useMemo(() => {}, [team]);
+  // const test = useMemo(() => {}, [team]);
 
   return <Page header="Builder Details">{game}</Page>;
 };
