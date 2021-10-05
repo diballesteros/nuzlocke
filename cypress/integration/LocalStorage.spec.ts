a234854157e55a6b106f0f443ece09aa29a0a936
Diego Ballesteros <diballesterosc@gmail.com>
2021-10-05 00:17:10 -0500

    Local storage test

---
+++ cypress/integration/LocalStorage.spec.ts
@@ -0,0 +1,24 @@
+describe('Local Storage', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.getLocalStorage('pokemon-tracker').then((data) => {
+      const addPokemon = data.replace('"pokemon":null', '"pokemon":{"value":1}');
+      const addRule = addPokemon.replace(
+        '"rules":{"Nuzlocke"',
+        '"rules":{"Test":[{"content":"Test"}], "Nuzlocke"'
+      );
+      const addRuleset = addRule.replace(
+        '"typeModal":null',
+        '"typeModal":null,"rulesets":[{"text": "Nuzlocke", "value":"Nuzlocke"}, {"text":"Test","value":"Test"}]'
+      );
+      const addFilter = addRuleset.replace('"pokemon":null', '"pokemon":null,"filter":["Test"]');
+      const changeVersion = addFilter.replace('"version":3', '"version":0');
+      cy.setLocalStorage('pokemon-tracker', changeVersion);
+    });
+  });
+
+  it('Migrate Zustand Version', () => {
+    cy.visit('/');
+    cy.contains('Nuzlocke Tracker').should('exist');
+  });
+});
