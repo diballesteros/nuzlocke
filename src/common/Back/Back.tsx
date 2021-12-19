fcc260673d8c76163deed604887c9b96211e275e
Yondiame <kassimbalogun16@gmail.com>
2021-12-19 21:28:15 +0100

    Fixed back button on smaller screens in the report page #285

--- src/common/Back/Back.tsx
+++ src/common/Back/Back.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import Button from 'semantic-ui-react/dist/commonjs/elements/Button';
 import Icon from 'semantic-ui-react/dist/commonjs/elements/Icon';
 import useStore from 'store';
+import styles from './Back.module.scss';
 
 function Back(): JSX.Element {
   const { t } = useTranslation('common');
@@ -15,9 +16,15 @@ function Back(): JSX.Element {
   };
 
   return (
-    <Button onClick={handleBack} style={{ height: 36 }} inverted={darkMode} type="button">
-      <Icon name="arrow left" />
-      {t('back')}
+    <Button
+      className={styles.button}
+      onClick={handleBack}
+      // style={{ height: 36 }}
+      inverted={darkMode}
+      type="button"
+    >
+      <Icon className={styles.icon} name="arrow left" />
+      <span className={styles.text}>{t('back')}</span>
     </Button>
   );
 }
