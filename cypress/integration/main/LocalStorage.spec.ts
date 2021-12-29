8b7b4fe544bc85c88d3b526f257eeafad8ad678d
Diego Ballesteros <diballesterosc@gmail.com>
2021-12-29 10:32:45 -0500

    More test fixes

--- cypress/integration/main/LocalStorage.spec.ts
+++ cypress/integration/main/LocalStorage.spec.ts
@@ -12,10 +12,13 @@ describe('Local Storage', () => {
         '"typeModal":null,"rulesets":[{"text": "Nuzlocke", "value":"Nuzlocke"}, {"text":"Test","value":"Test"}]'
       );
       const addFilter = addRuleset.replace('"pokemon":null', '"pokemon":null,"filter":["Test"]');
-      const replaceShiny = addFilter.replace('"status":null', '"status":{"value":6}');
+      const replaceShiny = addFilter.replace(
+        '"status":null',
+        '"status":{"value":6}, "details":{ "id":1,"level":1,"moves":[]}'
+      );
       const replaceSoulocke = replaceShiny.replace('Soulocke', 'SomethingElse');
       const replaceWedlocke = replaceSoulocke.replace('Wedlocke', 'SomethingElse');
-      const replaceBadge = replaceWedlocke.replace('"badge": []', '"badge": null');
+      const replaceBadge = replaceWedlocke.replace('"badge":[]', '"badge":null');
       const changeVersion = replaceBadge.replace('"version":7', '"version":0');
       cy.setLocalStorage('pokemon-tracker', changeVersion);
     });
@@ -26,7 +29,7 @@ describe('Local Storage', () => {
   it('Remake Brilliant Diamond and Shining Pearl', () => {
     cy.visit('/');
     cy.getLocalStorage('pokemon-tracker').then((data) => {
-      const removeBDSP = data.replace(/13.1/, '15');
+      const removeBDSP = data.replace(/13.1/gi, '15');
       const changeVersion = removeBDSP.replace('"version":7', '"version":4');
       cy.setLocalStorage('pokemon-tracker', changeVersion);
     });
