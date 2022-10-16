aba89283defcd71ea3b7dfe10844f573e9357e18
Diego Ballesteros <diballesteroscs@gmail.com>
2022-10-16 08:19:33 -0500

    fix: test fix

--- cypress/e2e/main/CustomGame.spec.ts
+++ cypress/e2e/main/CustomGame.spec.ts
@@ -8,14 +8,7 @@ describe('Custom game', () => {
     afterEach(() => {
       cy.get('[data-testid=add-encounter-input] > input').type('Test', { force: true });
       cy.contains('Save').click();
-      cy.get('[data-testid="open-scroll-list"]').click();
-      cy.get('[data-testid="skip-encounter-0"]').click();
-      cy.get('[data-testid="open-scroll-list"]').click();
-      cy.get('[data-testid="open-notes"]').click();
-      cy.get('[data-testid="game-notes"]')
-        .type('These are notes')
-        .should('have.value', 'These are notes');
-      cy.get('[data-testid="close-scroll-list"]').click();
+
       cy.get('[data-testid=filter] > input').type('Test');
       cy.wait(1000);
       cy.get('[data-testid=encounters-list]')
@@ -92,6 +85,14 @@ describe('Custom game', () => {
     cy.get('[data-testid=add-encounter]').click();
     cy.get('[data-testid=add-encounter-input] > input').type('Test');
     cy.contains('Save').click();
+    cy.get('[data-testid="open-scroll-list"]').click();
+    cy.get('[data-testid="skip-encounter-0"]').click();
+    cy.get('[data-testid="open-scroll-list"]').click();
+    cy.get('[data-testid="open-notes"]').click();
+    cy.get('[data-testid="game-notes"]')
+      .type('These are notes')
+      .should('have.value', 'These are notes');
+    cy.get('[data-testid="close-scroll-list"]').click();
     cy.get('[data-testid=reset-all]').click();
     cy.contains('Save').click();
     cy.get('[data-testid=encounters-list]')
