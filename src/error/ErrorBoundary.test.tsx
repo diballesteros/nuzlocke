f8d2a5da2e655191d6a226cc9878a14caca2a177
Diego Ballesteros <diballesteroscs@gmail.com>
2022-10-16 09:24:48 -0500

    fix: removed useless import

--- src/error/ErrorBoundary.test.tsx
+++ src/error/ErrorBoundary.test.tsx
@@ -1,4 +1,3 @@
-import * as Sentry from '@sentry/react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import CalcErrorboundary from './CalcErrorBoundary';
