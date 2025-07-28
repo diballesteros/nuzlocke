4b707babc3df022b1c1dcb1aec822ea279eef27c
Diego Ballesteros <diballesteroscs@gmail.com>
2025-07-28 13:30:34 -0500

    feat(6.0.0): updated packages

--- cypress.config.ts
+++ cypress.config.ts
@@ -10,11 +10,6 @@ export default defineConfig({
   },
   e2e: {
     baseUrl: 'http://localhost:5173',
-    setupNodeEvents(on, config) {
-      require('@cypress/code-coverage/task')(on, config);
-
-      return config;
-    },
     specPattern: 'cypress/e2e/**/*.spec.ts',
   },
 });
