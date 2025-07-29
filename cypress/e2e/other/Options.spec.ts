968710b500ebcc1b491a25c9d877e3e8d7739841
Diego Ballesteros <diballesteroscs@gmail.com>
2025-07-28 19:01:54 -0500

    feat(6.0.0): removing prefer dark mode test

--- cypress/e2e/other/Options.spec.ts
+++ cypress/e2e/other/Options.spec.ts
@@ -124,20 +124,6 @@ describe('Options', () => {
     cy.get('[data-testid="app"]').should('have.css', 'background-color', 'rgb(33, 33, 33)');
   });
 
-  it('Prefers Dark Mode', () => {
-    cy.visit('/', {
-      onBeforeLoad: (win) => {
-        cy.stub(win, 'matchMedia')
-          .withArgs('(prefers-color-scheme:dark)')
-          .returns({
-            matches: true,
-            addListener: () => {},
-          });
-      },
-    });
-    cy.get('[data-testid="app"]').should('have.css', 'background-color', 'rgb(33, 33, 33)');
-  });
-
   it('Language', () => {
     cy.contains('Settings').click();
     cy.get('[data-testid="settings-language"]').click();
